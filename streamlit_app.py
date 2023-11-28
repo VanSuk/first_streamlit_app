1e2749a0f82e8a9c0fe805374bb9f84a09565bbd
VanSuk <152315071+VanSuk@users.noreply.github.com>
2023-11-28 11:23:04 -0600

    Update streamlit_app.py
    Added another header

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,5 @@ streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🥑🍞 Avacado Toast')
+
+streamlit.header(''🍌🥭 Build Your Own Fruit Smoothie 🥝🍇'')
